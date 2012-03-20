0202f513b294764d8cf7b2c57fd336f9928a93a0
Neal Sales-Griffin <nealsalesgriffin@gmail.com>
2012-03-19 21:41:43 -0500

    added a static pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ SampleApp::Application.routes.draw do
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
   
+  match '/users',   to: 'users#index'
   match '/signup',  to: 'users#new'
   match '/signin',  to: 'sessions#new'
   match '/signout',  to: 'sessions#destroy'
